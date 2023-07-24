8985e68265530d8eba0678fc7022c7ff817b39ec
akinitune <akinitune05@gmail.com>
2023-07-24 11:48:30 -0500

    first commit

---
+++ homepage.js
@@ -0,0 +1,11 @@
+function relax(){
+document.getElementById('cover').style.visibility="hidden";
+document.getElementById("menux").style.visibility="hidden"
+document.body.style.backgroundColor="white"
+
+}
+function constrict(){
+    document.getElementById('cover').style.visibility="visible";
+    document.getElementById("menux").style.visibility="visible"
+    document.body.style.backgroundColor="rgb(190, 187, 200)"
+    }
